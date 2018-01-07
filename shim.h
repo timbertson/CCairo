b7244900f2087c57bd7be0c50a36f7f2b68e536b
Tim Cuthbertson <tim@gfxmonk.net>
2018-01-07 21:47:29 +1100

    include cairo-quartz on OSX

--- shim.h
+++ shim.h
@@ -2,3 +2,10 @@
 #include <cairo-pdf.h>
 #include <cairo-svg.h>
 #include <cairo-ft.h>
+
+#if __APPLE__
+  #include "TargetConditionals.h"
+  #if TARGET_OS_MAC
+    #include <cairo-quartz.h>
+  #endif
+#endif
